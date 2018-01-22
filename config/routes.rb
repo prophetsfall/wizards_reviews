c5460d1fbd90b7bd15b8e6e1ea04d6915c4f952a
James Hartman <hartman.james.p@gmail.com>
2018-01-22 15:19:25 -0500

    added wizard api route

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   root 'homes#index'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :wizards, only: [:index, :show]
+    end
+  end
 end
