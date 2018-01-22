e67ebe7e6496182873289cf770f4a992bb94714f
alcodes299 <alskis299@gmail.com>
2018-01-22 15:09:30 -0500

    added api fetch request

--- app/javascript/react/containers/WizardsContainer.js
+++ app/javascript/react/containers/WizardsContainer.js
@@ -4,10 +4,23 @@ class WizardsContainer extends Component {
   constructor(props){
     super(props);
     this.state={
+      wizards: []
+
+    }
+    componentDidMount() {
+
+    // FETCH ARTICLE BY ID
+    fetch(`api/v1/index`)
+    .then(response => response.json())
+    .then(json => {
+
+      this.setState({ wizards: json})
+    })
+  }
 
     }
   }
   render(){
-    
+
   }
 }
