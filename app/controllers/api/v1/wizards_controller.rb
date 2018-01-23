9f051f6e72c7728700e53b4df026aebfe65ad102
Patrick Whelan <patrickawhelan@gmail.com>
2018-01-23 15:15:21 -0500

    removed extraneous def authorize user methoed

--- app/controllers/api/v1/wizards_controller.rb
+++ app/controllers/api/v1/wizards_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::WizardsController < ApplicationController
   def index
-    render json: Wizard.all # this needs to be returning our wizard reviews as well
+    render json: Wizard.all 
   end
 end
