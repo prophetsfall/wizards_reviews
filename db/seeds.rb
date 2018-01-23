ccfae8ac6344de4290f4dfd52d5813489d2819db
alcodes299 <alskis299@gmail.com>
2018-01-23 11:23:32 -0500

    Seeeded database with some wizards

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Wizard.create!(name: "Gandalf the White", description: "Hes got to be top then mabye 5")
+Wizard.create!(name: "Gandalf the Grey", description: "Hes great and all but he hidding")
+Wizard.create!(name: "Gwydion", description: "Hes norse")
+Wizard.create!(name: "Merlin", description: "Super famouse wizard")
+Wizard.create!(name: "Jafar", description: "Hes from aladdin")
+Wizard.create!(name: "Maugris", description: "Hes enchants matter in france")
+Wizard.create!(name: "Saruman", description: "Hes fights Gandalf")
