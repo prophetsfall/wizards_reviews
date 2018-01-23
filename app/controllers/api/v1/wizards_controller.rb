3772ba6a7b9a392495abdb3742143569e5652ddd
Patrick Whelan <patrickawhelan@gmail.com>
2018-01-23 16:34:08 -0500

    includes spec for wizards controller with show.

--- app/controllers/api/v1/wizards_controller.rb
+++ app/controllers/api/v1/wizards_controller.rb
@@ -1,5 +1,10 @@
 class Api::V1::WizardsController < ApplicationController
   def index
-    render json: Wizard.all 
+    render json: Wizard.all
+  end
+
+  def show
+    @wizard = Wizard.find(params[:id])
+    render json: { wizard: @wizard, reviews: @wizard.reviews }
   end
 end
--- spec/controllers/api/v1/wizards_controller_spec.rb
+++ spec/controllers/api/v1/wizards_controller_spec.rb
@@ -1,31 +1,56 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::WizardsController, type: :controller do
+  let!(:user1) {FactoryBot.create(:user)}
   let!(:clippy) { Wizard.create(
     name: "Clippy",
     description: "Unacceptably helpful word wizard",
     img_url: "www.clippy.com",
-    creator_id: 2
+    creator_id: user1.id
+    )
+  }
+
+  let!(:clippy_review) { Review.create(
+    user_id: user1.id,
+    wizard_id: clippy.id,
+    body: "Clippy is not ideal.",
+    rating: 20
     )}
+
   let!(:install) { Wizard.create(
     name: "Microsoft",
     description: "Installation Wizard",
     img_url: "www.microsoft.com",
-    creator_id: 2
-  )}
+    creator_id: user1.id
+    )}
 
-  describe "GET#index" do
-    it "should return our wizards" do
-      get :index
-      returned_json = JSON.parse(response.body)
+    describe "GET#index" do
+      it "should return our wizards" do
+          sign_in :user, user1
+        get :index
+        returned_json = JSON.parse(response.body)
 
-      expect(response.status).to eq 200
-      expect(response.content_type).to eq("application/json")
+        expect(response.status).to eq 200
+        expect(response.content_type).to eq("application/json")
 
-      expect(returned_json.length).to eq 2
-      expect(returned_json[0]["name"]).to eq "Clippy"
-      expect(returned_json[0]["description"]).to eq "Unacceptably helpful word wizard"
-      expect(returned_json[1]["name"]).to eq "Microsoft"
+        expect(returned_json.length).to eq 2
+        expect(returned_json[0]["name"]).to eq "Clippy"
+        expect(returned_json[0]["description"]).to eq "Unacceptably helpful word wizard"
+        expect(returned_json[1]["name"]).to eq "Microsoft"
+      end
+    end
+
+
+
+    describe "GET#show" do
+      it "should return our review" do
+        sign_in :user, user1 
+        get :show, params:{ id: clippy.id }
+        returned_json = JSON.parse(response.body)
+
+        expect(response.status).to eq 200
+        expect(response.content_type).to eq("application/json")
+        expect(returned_json)
+      end
     end
   end
-end
