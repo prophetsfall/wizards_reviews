84ad527ae383d3f6e304c886abd08e520c929b43
alcodes299 <alskis299@gmail.com>
2018-01-25 13:11:40 -0500

    added erb form for wizards

--- app/controllers/wizards_controller.rb
+++ app/controllers/wizards_controller.rb
@@ -2,4 +2,30 @@ class WizardsController < ApplicationController
   before_action :authenticate_user!, except:[:index, :show]
 
   def index; end
+  def new
+    if current_user
+      @wizard = Wizard.new
+    else
+      redirect_to :index
+    end
+  end
+  def create
+    @wizard = Wizard.new(wizard_params)
+    if current_user
+      if @wizard.save
+        redirect_to wizard_path(@wizard)
+        flash[:notice] = Wizards added successfully”
+      else
+        flash[:notice] = Wizards add failed”
+        render :new
+      end
+    else
+      flash[:notice] = "You must be signed in!"
+      render :new
+    end
+  protected
+
+  def wizards_params
+    params.require(:wizard).permit(:name, :description, :img_url, :creator_id)
+  end
 end
