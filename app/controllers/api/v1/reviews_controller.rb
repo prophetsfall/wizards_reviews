084b0925c45cac21f65d0c741a171efd48fbc343
Taylor Yount <tayloryount@Taylors-MacBook-Air.local>
2018-01-25 14:58:35 -0500

    added update route for reviews

--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -5,25 +5,29 @@ class Api::V1::ReviewsController < ApplicationController
     if user_signed_in?
       # Design descision to be made: will the react element need the wizard?
       if new_review.save
-        render json: { wizard: wizard , review: review_params }
+        render json: { wizard: wizard, review: review_params }
       else
-        render json: { wizard: wizard , review: review_params, errors: new_review.errors.full_messages }, status: :unprocessable_entity
+        render json: { wizard: wizard, review: review_params, errors: new_review.errors.full_messages }, status: :unprocessable_entity
       end
     else
-      render json: { wizard: wizard , review: review_params, errors: "Access Denied" }, status: :unprocessable_entity
+      render json: { wizard: wizard, review: review_params, errors: "Access Denied" }, status: :unprocessable_entity
     end
   end
 
-  def edit
+  def update
+
     review = Review.find(review_params[:id])
-    if current_user.id == review_params[:user_id]
-      if review.update
-        render json: { wizard: wizard , review: review_params }
+    if current_user.id == review_params[:user_id].to_i
+
+      if review.update(review_params)
+        render json: { review: review_params }
       else
-        render json: { wizard: wizard , review: review_params, errors: new_review.errors.full_messages }, status: :unprocessable_entity
+
+        render json: { review: review_params, errors: new_review.errors.full_messages }, status: :unprocessable_entity
       end
     else
-      render json: { wizard: wizard , review: review_params, errors: "Access Denied" }, status: :unprocessable_entity
+
+      render json: { review: review_params, errors: "Access Denied" }, status: :unprocessable_entity
     end
   end
 
--- spec/controllers/api/v1/reviews_controller_spec.rb
+++ spec/controllers/api/v1/reviews_controller_spec.rb
@@ -24,7 +24,7 @@ RSpec.describe Api::V1::ReviewsController, type: :controller do
       expect(returned_json["wizard"]["name"]).to eq "Clippy"
       expect(returned_json["review"]["body"]).to eq "Clippy is not ideal."
   end
-    it 'should return a json object with erros and a 422 response if a field is empty' do
+    it 'should return a json object with errors and a 422 response if a field is empty' do
       sign_in :user, user2
 
       post :create, params: { review: { user_id: user2.id, wizard_id: clippy.id, body: '', rating: '' } }
@@ -46,6 +46,22 @@ RSpec.describe Api::V1::ReviewsController, type: :controller do
       expect(returned_json["errors"]).to eq "Access Denied"
       expect(returned_json["review"]["body"]).to eq "Clippy is not ideal."
     end
+  describe 'PATCH#update' do
+    it 'should return a json object with our updated review' do
+      sign_in :user, user2
+      review1 = Review.create!(user_id: user2.id, wizard_id: clippy.id, body: "asdfjkl;", rating: 40 )
+
+      # post :create, params: { review: { user_id: user2.id, wizard_id: clippy.id, body: "Clippy is not ideal.", rating: 20 } }
+      patch :update, params: { id: review1.id, review: { id: review1.id, user_id: user2.id, wizard_id: clippy.id, body: "cow", rating: 20 } }
+
+      returned_json = JSON.parse(response.body)
+      expect(response.status).to eq 200
+      expect(response.content_type).to eq("application/json")
+
+      expect(returned_json["review"]["body"]).to eq "cow"
+
+    end
+  end
   end
 
 end
