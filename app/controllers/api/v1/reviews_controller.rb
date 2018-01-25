06d3ba418dc30896e019394f89d846256b6529e2
Ian Brooks <ianphilipbrooks@gmail.com>
2018-01-25 16:00:12 -0500

    Removed returning of wizard in the create response removed returning of wizard and reviews if user is no signed in and tries to create updated error code to :forbidden switched to using user_id found in DB removed user_id from sterilized params

--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -1,22 +1,21 @@
 class Api::V1::ReviewsController < ApplicationController
   def create
-    new_review = Review.new(review_params)
     wizard = Wizard.find(review_params[:wizard_id])
     if user_signed_in?
-      # Design descision to be made: will the react element need the wizard?
+      new_review = Review.new(review_params.merge({user_id: current_user.id}))
       if new_review.save
-        render json: { wizard: wizard , review: review_params }
+        render json: { review: review_params }
       else
-        render json: { wizard: wizard , review: review_params, errors: new_review.errors.full_messages }, status: :unprocessable_entity
+        render json: { review: review_params, errors: new_review.errors.full_messages }, status: :unprocessable_entity
       end
     else
-      render json: { wizard: wizard , review: review_params, errors: "Access Denied" }, status: :unprocessable_entity
+      render json: { errors: "Access Denied" }, status: :forbidden
     end
   end
 
   def edit
     review = Review.find(review_params[:id])
-    if current_user.id == review_params[:user_id]
+    if current_user.id == review.user_id
       if review.update
         render json: { wizard: wizard , review: review_params }
       else
@@ -30,6 +29,6 @@ class Api::V1::ReviewsController < ApplicationController
   protected
 
   def review_params
-    params.require(:review).permit(:body, :wizard_id, :user_id, :rating, :id)
+    params.require(:review).permit(:body, :wizard_id, :rating, :id)
   end
 end
