3b72490b6402a84b9ab0f324a5c4497f64210f78
Patrick Whelan <patrickawhelan@gmail.com>
2018-01-26 10:44:49 -0500

    cleaned up extra controller actions

--- app/controllers/api/v1/wizards_controller.rb
+++ app/controllers/api/v1/wizards_controller.rb
@@ -2,19 +2,6 @@ class Api::V1::WizardsController < ApplicationController
   def index
     render json: Wizard.all
   end
-  def create
-    new_wizard = Wizard.new(wizard_params)
-
-    if current_user.admin?
-      if new_wizard.save
-        render json: { wizard: new_wizard, reviews: [] }
-      else
-        render json: { errors: new_wizard.errors.full_messages }, status: :unprocessable_entity
-      end
-    else
-      render json: { errors: "Access Denied" }, status: :unprocessable_entity
-    end
-  end
 
   def show
     @wizard = Wizard.find_by(name: params[:id])
