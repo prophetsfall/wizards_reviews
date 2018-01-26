5b4352c6674cddfa7765f0cfd2f6245e33650432
Patrick Whelan <patrickawhelan@gmail.com>
2018-01-26 13:53:51 -0500

    Added button to new wizard form. Added tests to ensure user can push button and create wizard.

---
+++ spec/features/user_clicks_new_wizard_and_creates_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+feature 'user clicks new wizard and creates new wizard', %Q{
+  As a signed in user
+  I want to click new wizard
+  So that I can create a new wizard
+} do
+  scenario 'specify valid credentials' do
+    user = FactoryBot.create(:user)
+
+    visit new_user_session_path
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+
+    click_button 'Log in'
+    expect(page).to have_content("Signed in successfully")
+
+    click_button 'New Wizard'
+    expect(page).to have_content("Name")
+    expect(page).to have_content("Description")
+    expect(page).to have_content("New Wizard Form")
+  end
+
+  scenario 'creates a wizard' do
+    user = FactoryBot.create(:user)
+
+    visit new_user_session_path
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+
+    click_button 'Log in'
+
+    click_button 'New Wizard'
+    fill_in 'Name', with: "Mortimar"
+    fill_in 'Description', with: "Mage Most Magnificint"
+
+    click_button 'Add Wizard'
+    expect(page).to have_content("Wizard added successfully")
+  end
+end
