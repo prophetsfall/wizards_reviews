3f0abafc0d8ce49bbae8e8bd679673f8353e2b21
Ian Brooks <ianphilipbrooks@gmail.com>
2018-01-30 16:35:07 -0500

    added relations for vote to user and review

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :wizard
-
+  has_many :votes
   validates :body, presence: true
   validates :rating, presence: true, numericality: {
     greater_than_or_equal_to: 1,
