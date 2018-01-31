c3a9e2195b451612d72304da644c459f18b20036
Ian Brooks <ianphilipbrooks@gmail.com>
2018-01-31 10:50:44 -0500

    removed erroneous storage setting added region to config

--- app/uploaders/profile_photo_uploader.rb
+++ app/uploaders/profile_photo_uploader.rb
@@ -10,7 +10,6 @@ class ProfilePhotoUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
