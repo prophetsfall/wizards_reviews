c2f09a6b12fefe387901dccb5cdfaae822bdf3c4
James Hartman <hartman.james.p@gmail.com>
2018-02-01 11:09:33 -0500

    hooked up upvotes in react

--- app/javascript/react/components/ReviewTile.js
+++ app/javascript/react/components/ReviewTile.js
@@ -6,11 +6,34 @@ const ReviewTile = props => {
   let body = props.body
   let rating = props.rating
   let created_at = props.created_at
+  let votes = props.votes
+
+  let handleClick = (event) => {
+    let clicked = event.target.id
+    let formPayload
+    if (clicked === 'upvote'){
+      formPayload = {
+        vote: {
+          vote: 1,
+          review_id: props.id
+        }
+      }
+    } else if (clicked === 'downvote') {
+      formPayload = {
+        vote: {
+          vote: -1,
+          review_id: props.id
+        }
+      }
+    }
+    props.alterVote(formPayload)
+  }
 
   return(
     <div>
       <p> {body}</p>
       <p> {rating}</p>
+      <p><i id="downvote" className="fas fa-arrow-down" onClick={handleClick}>down</i> {votes} <i id="upvote" className="fas fa-arrow-up" onClick={handleClick}>up</i></p>
     </div>
   )
 }
--- app/javascript/react/containers/WizardShowContainer.js
+++ app/javascript/react/containers/WizardShowContainer.js
@@ -13,6 +13,7 @@ class WizardShowContainer extends Component {
       reviews: []
     }
     this.addNewReview = this.addNewReview.bind(this)
+    this.alterVote = this.alterVote.bind(this)
   }
 
   componentDidMount() {
@@ -62,14 +63,52 @@ class WizardShowContainer extends Component {
     .catch(error => console.error(`Error in fetch: ${error.message}`));
   }
 
+  alterVote(formPayload) {
+    fetch('/api/v1/votes', {
+      credentials: 'same-origin',
+      method: 'post',
+      body: JSON.stringify(formPayload),
+      headers: {
+        'Content-Type': 'application/json',
+        'X-Requested-With': 'XMLHttpRequest',
+        'X-CSRF-Token': $('meta[name=csrf-token]').attr('content')
+      }
+    })
+    .then(response => {
+      if (response.ok) {
+        return response;
+      } else {
+        let errorMessage = `${response.status} (${response.statusText})`,
+            error = new Error(errorMessage);
+        throw(error);
+      }
+    })
+    .then(response => response.json())
+    .then(body => {
+      let alteredReviews
+      alteredReviews = this.state.reviews.map((review) => {
+        if (review.id === body.review.id){
+          review.vote_count = body.review.vote_count
+        }
+        return(review)
+      })
+      this.setState({ reviews: alteredReviews })
+    })
+    .catch(error => console.error(`Error in fetch: ${error.message}`));
+  }
+
   render() {
     let reviewArray = this.state.reviews.map((review) => {
+
       return(
         <ReviewTile
           key={review.id}
+          id={review.id}
           body={review.body}
           rating={review.rating}
           userName={review.creator_name}
+          votes={review.vote_count}
+          alterVote={this.alterVote}
         />
       )
     })
